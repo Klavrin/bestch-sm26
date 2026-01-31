e5307fe0a6b024f007e14c67da798e461d7203c2
Sergiu Gherasim <sergeras4@gmail.com>
2026-01-31 02:01:11 +0200

    refactor: remove unnecessary routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import i18next from 'i18next'
 import Header from './components/header'
 import Footer from './components/footer'
 import LandingPage from './pages/landing-page'
-import WebDevelopmentPage from './pages/web-development-page'
-import WebDesignPage from './pages/web-design-page'
-import SeoPage from './pages/seo-page'
-import MaintenanceAndSupportPage from './pages/maintenance-page'
-import PrivacyPolicyPage from './pages/privacy-policy-page'
 import PageNotFound from './pages/404-page'
 import InteractiveCursorGSAP from './components/interactive-cursor-gsap'
 import Dialog from './components/dialog'
@@ -35,11 +30,11 @@ const App = () => {
         <Dialog />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="web-development" element={<WebDevelopmentPage />} />
-          <Route path="web-design" element={<WebDesignPage />} />
-          <Route path="maintenance-&-support" element={<MaintenanceAndSupportPage />} />
-          <Route path="seo" element={<SeoPage />} />
-          //<Route path="privacy-policy" element={<PrivacyPolicyPage />} />
+          {/* <Route path="web-development" element={<WebDevelopmentPage />} /> */}
+          {/* <Route path="web-design" element={<WebDesignPage />} /> */}
+          {/* <Route path="maintenance-&-support" element={<MaintenanceAndSupportPage />} /> */}
+          {/* <Route path="seo" element={<SeoPage />} /> */}
+          {/* <Route path="privacy-policy" element={<PrivacyPolicyPage />} /> */}
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
