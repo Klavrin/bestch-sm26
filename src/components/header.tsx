da53ae6134dfc2da93f2fd8f6a3d13a6217637f1
drlh <drlh@tutanota.com>
2026-02-12 01:00:28 +0200

    bug: Fixed scaling on smartphones

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
 
   return (
     <header className="w-screen absolute z-40 transform-gpu">
-      <div className="max-w-[1400px] h-[120px] mx-auto xl:px-[75px] px-[18px] text-black flex justify-between items-center transition-[padding] duration-500">
+      <div className="max-w-[1400px] h-[120px] mx-auto xl:px-[75px] px-[18px] text-black flex justify-between items-center transition-[padding] duration-500 gap-4">
         <div>
           <Link
             to="/"
@@ -43,7 +43,7 @@ const Header = () => {
             onMouseOver={() => setCursorSize(100)}
             onMouseLeave={() => setCursorSize(40)}
           >
-            <div className="flex items-center gap-2 w-auto transition-transform duration-300 origin-left scale-100 md:scale-105 lg:scale-110 xl:scale-115">
+            <div className="flex items-center gap-2 w-auto transition-transform duration-300 origin-left scale-75 sm:scale-95 md:scale-105 lg:scale-115 xl:scale-125">
               <img
                 src={BestCoursesImage}
                 alt="Best Courses"
