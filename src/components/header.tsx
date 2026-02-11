b53e3289398a5d82a3c58d826ed95a763f85ae71
drlh <drlh@tutanota.com>
2026-02-12 00:26:21 +0200

    bug: Fixed images by using their own height. Might backfire though

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,23 +36,23 @@ const Header = () => {
   return (
     <header className="w-screen absolute z-40 transform-gpu">
       <div className="max-w-[1400px] h-[120px] mx-auto xl:px-[75px] px-[18px] text-black flex justify-between items-center transition-[padding] duration-500">
-        <div className="sm:static absolute left-0 sm:w-auto w-screen text-center">
+        <div className="sm:static sm:left-0 absolute right-0 sm:-translate-x-0 -translate-x-1/2 transition-transform duration-300">
           <Link
             to="/"
             className="inline-block hover:opacity-80 transition-opacity"
             onMouseOver={() => setCursorSize(100)}
             onMouseLeave={() => setCursorSize(40)}
           >
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 w-auto transition-transform duration-300">
               <img
                 src={BestCoursesImage}
                 alt="Best Courses"
-                className="xl:h-[100px] lg:h-[80px] md:h-[70px] h-[50px] w-auto transition-all duration-300"
+                className="transition-all duration-300"
               />
               <img
                 src={BestCh}
                 alt="Best Summer Course"
-                className="xl:h-[100px] lg:h-[80px] md:h-[70px] h-[50px] w-auto transition-all duration-300"
+                className="transition-all duration-300"
               />
             </div>
           </Link>
