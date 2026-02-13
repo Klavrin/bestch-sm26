a41e711b2b588e58cf5be28a44927d207f377437
drlh <drlh@tutanota.com>
2026-02-13 19:26:55 +0200

    just some spacing

--- src/components/gradient-title.tsx
+++ src/components/gradient-title.tsx
@@ -18,22 +18,22 @@ export default function GradientTitle({
   return (
     <h1
       className={`
-          font-humane
-          font-bold
-          text-[20vw]
-          xl:text-[18vw]
-          leading-[0.84]
-          -mb-[2vw]
-          whitespace-nowrap
-          bg-[radial-gradient(68.71%_1291.01%_at_50.05%_50%,#FF9800_18.75%,#EB7400_99.96%,rgba(0,0,0,0.68)_99.97%)]
-          bg-clip-text
-          text-transparent
-          ${className}
-        `}
-        onMouseOver={() => setCursorSize?.(onHoverSize)}
-        onMouseLeave={() => setCursorSize?.(onLeaveSize)}
-      >
-        {children}
-      </h1>
+        font-humane
+        font-bold
+        text-[20vw]
+        xl:text-[18vw]
+        leading-[0.84]
+        -mb-[2vw]
+        whitespace-nowrap
+        bg-[radial-gradient(68.71%_1291.01%_at_50.05%_50%,#FF9800_18.75%,#EB7400_99.96%,rgba(0,0,0,0.68)_99.97%)]
+        bg-clip-text
+        text-transparent
+        ${className}
+      `}
+      onMouseOver={() => setCursorSize?.(onHoverSize)}
+      onMouseLeave={() => setCursorSize?.(onLeaveSize)}
+    >
+      {children}
+    </h1>
   );
 }
